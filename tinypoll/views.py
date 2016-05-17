16397bdfe43100c60c3fa26203829203649432c3
Jaewook Woo <vbgb0926@gmail.com>
2016-05-17 22:02:41 +0900

    Increase votes

--- tinypoll/views.py
+++ tinypoll/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from .models import Poll, Option
+from . import app, db
 
 blueprint = Blueprint('tinypoll', __name__)
 
@@ -13,3 +14,10 @@ def poll(poll_id):
     poll = Poll.query.get(poll_id)
     options = Option.query.filter_by(poll_id=poll_id).all()
     return render_template("poll.html", poll=poll, options=options)
+
+@blueprint.route('/<int:poll_id>/<int:option_id>')
+def option(poll_id, option_id):
+	option = Option.query.get(option_id)
+	option.votes += 1
+	db.session.commit()
+	return redirect(url_for('tinypoll.index'))
